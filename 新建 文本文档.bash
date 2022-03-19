5aa330a3d4db1c81afdc2b60bb8386eefde5a783
mr-liu1996 <1637494149@qq.com>
2022-03-19 22:02:20 +0800

    main

---
+++ 新建 文本文档.bash
@@ -0,0 +1,153 @@
+ docker run -d \
+  --name=DDNSTO \
+  --restart always \
+  -e TOKEN=504b9918-57d5-4fde-a7c7-07553779e3a5 \
+  -e DEVICE_IDX=0\
+  -v /etc/localtime:/etc/localtime:ro \
+  -e PUID=0 \
+  -e PGID=0 \
+  linkease/ddnsto
+
+
+服务器配置
+
+启用 备注
+宽带
+ 协议
+Vmess
+ 端口
+5373
+ ID/密码
+
+ad91de82-a7e1-4109-b1ec-85abd6ce6907
+
+ 额外ID(AlterID)
+0
+ 用户等级(Level)
+1
+ TLS 传输方式
+TCP
+ 伪装类型
+http
+ HTTP主机
+a.189.cn
+
+青龙重要参数：
+Client ID fU__2oeB6Qqb
+Client Secret 365O8JH-3b88iy_Tskb2Ekyq
+
+**************************************************
+1982181942:AAHsb3a4YP79xUl3mk4y94lh7NwkA96lz7U   
+1674195732 @liucn_bot
+
+Here is the token for bot jhvhj @mrliu_bot:
+1245607115:AAG4qd23NAREOmicYWVq0O9TNOPm7pnVAI0
+1036459749
+
+**************************************************
+
+
+ltphone 18555894518@17625207115
+ltpwd 199612@199612
+
+**************************************************
+export DD_BOT_TOKEN="81b6e6e5e503a97baa17a3b394bd53ef84584860153fb8936894ec87dc9551f3"
+export DD_BOT_SECRET="SEC949df0e76126809ae1f58fc2f5fe8eadafc80a6569cd5798d9aafd7af320b489"
+
+export TG_BOT_TOKEN="1982181942:AAHsb3a4YP79xUl3mk4y94lh7NwkA96lz7U"
+## 下方填写 @getuseridbot 中获取到的纯数字ID
+export TG_USER_ID="1674195732"
+
+export SMZDM_COOKIE="sess=AT-mP%2B10wz3L%2BA%2B2zgN8YHjL3oa84UdMi4SCKpQ12kjUjUkwOdlVKgZrEEM%2FEWCSqFDtle%2BG3ZbhksY1OlzMmQiqmsg9OHMptmEBnH3mQBYWQ0btM2T5b0JQLM%3D; user=user%3A7372155326%7C7372155326;"
+## 天翼
+export TIANYI="17625207115-Aa199612"
+## mt论坛
+export MT_INFO="wxl1637494149-Aa199612"
+## 小米运动
+export XIAOMI="17625207115-Aa199612&18555894515-Aa199612"
+## 科学刀
+export KXDAO_COOKIE="GG1NZ_2132_saltkey=yqv9JqW1; G1NZ_2132_auth=658aeWFNIKsjw3rwip%2FKA2aJQdKWaZOi7ku7rSprMWpThpH8xjMop1xM9FRDfOVY5ooiVSM5cpenoDqQ6yY0rpNNag;"
+## 科技玩家
+export KJWJ_UP="1637494149@qq.com-Aa199612"
+## 恩山论坛
+export ENSHAN_COOKIE="DJyZ_2132_saltkey=H5S12FR3;DJyZ_2132_auth=235dds2Vc3B4t4zwUnVBR9vBJmhJaT83i2QLVbOvtUw007hY2zPz9%2FBlkkXs3CWqe0kTGENJ8jbJzbZFaocvqw53UrU;"
+## 吾爱破解
+## export WUAI_COOKIE="htVD_2132_saltkey=qBoAAdwm; htVD_2132_auth=949ftzf17diCPiSwpldKi%2BUrKr6gxLolu3ZT68pQOZn9Ro8dHTD3kkPokuoba5zLiqjWRG9W8Z3DQzROg8iN3Q2kUUk;"
+## 聚看点
+export jkdhd='{"openid": "9d0ffe11de4d47c0900461311b71e7c4"}'@'{"openid": "9f8794e0fc93413881f06c4c478ff0e0"}'
+export jkdck='{"Cookie":"xz_jkd_appkey=9d0ffe11de4d47c0900461311b71e7c4"}'@'{"Cookie":"xz_jkd_appkey=9f8794e0fc93413881f06c4c478ff0e0"}'
+export htthd='{"loginId":"9f49bb92d0014030aaf0de5fb5a405f5","userId":"153788748"}'
+export guaopencard60='true'
+export guaopencard59='true'
+export guaopencard57='true'
+export FRUITSHARECODES="b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5&b33a556418ee4ce09136c99f18ce0771@87ec63320c2a48f78272532c774684e5"
+export PETSHARECODES="MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ==&MTE0MDQ3MzEwMDAwMDAwNDc1NzgxNDM=@MTAxODc2NTEzNDAwMDAwMDAyMjcyNDUzNQ=="
+export PLANT_BEAN_SHARECODES="u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy&u72q4vdn3zes2xiqesuj7cwgizqu7a47nrexgvy@mgotg3ifqaafdsd3crk5xlxym5ybyl62asqzvjy"
+export DDFACTORY_SHARECODES="T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVWnYaS5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVWnYaS5kRrbA"
+export DREAM_FACTORY_SHARE_CODES="vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA==&vzQL9RqQ8i_41HDxtzqtoA=="
+export JDSGMH_SHARECODES="T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA&T0205KkcGEljoy-1YkaFwa1uCjVQmoaT5kRrbA@T020a17mL1dAI_NAVGuKTH2RCjVQmoaT5kRrbA"
+export JD_CASH_SHARECODES="eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI&eU9YNLvIMIdEszKknil3@9riiA6XrsFuxhR-rE_mI"
+export GOTIFY_URL="http://49.79.36.92:5705"
+export GOTIFY_TOKEN="uF3-YOXv7DCC"
+export GOTIFY_PRIORITY="666"
+
+*******************************************************************************
+芝士仓库 ql repo https://github.com/cdle/carry.git "jd_" "" "jdCookie.js|sendNotify.js|share_code.js|USER_AGENTS.js" 0 * * * *
+
+知名大佬仓库 ql repo https://github.com/shufflewzc/faker2.git "jd_|jx_|gua_|jddj_|getJDCookie" "activity|backUp" "^jd[^_]|USER|function|utils|sendnotify|ZooFaker_Necklace.js|JDJRValidator_|sign_graphics_validate|QL"
+
+欢太仓库 ql repo https://github.com/Mashiro2000/HeyTapTask.git "" "Backup|index|HT.*|sendNotify" "HT.*|sendNotify"
+
+hellow仓库 ql repo https://github.com/JDHelloWorld/jd_scripts.git
+
+大佬仓库 ql repo https://github.com/yuannian1112/jd_scripts.git "jd_|jx_|getJDCookie" "activity|backUp" "^jd[^_]|USER|utils"
+
+wskey仓库 ql repo https://github.com/Zy143L/wskey.git "wskey"
+
+仓库 ql repo https://github.com/Tsukasa007/my_script.git "" "jdCookie|USER_AGENTS|sendNotify|backup" "" "master"
+
+dingtalkBotToken
+81b6e6e5e503a97baa17a3b394bd53ef84584860153fb8936894ec87dc9551f3
+钉钉机器人webhook token，例如：5a544165465465645d0f31dca676e7bd07415asdasd
+dingtalkBotSecret
+SEC949df0e76126809ae1f58fc2f5fe8eadafc80a6569cd5798d9aafd7af320b489
+
+
+***************************************************************
+
+***************************************************************
+
+docker run -dit \
+  -v /volume1/docker/ql/config:/ql/config \
+  -v /volume1/docker/ql/log:/ql/log \
+  -v /volume1/docker/ql/db:/ql/db \
+  -v /volume1/docker/ql/scripts:/ql/scripts \
+  -v /volume1/docker/ql/jbot:/ql/jbot \
+  -v /volume1/docker/ql/repo:/ql/repo \
+  -v /volume1/docker/ql/raw:/ql/raw \
+  -v /volume1/docker/ql/deps:/ql/deps \
+  -v /volume1/docker/ql/ninja:/ql/ninja \
+  -p 5700:5700 \
+  -p 5701:5701 \
+  --name ql \
+  --hostname ql \
+  --restart always \
+  whyour/qinglong:latest
+  
+docker run -d --name jellyfin \
+	-v $PWD/jellyfin1/config:/config \
+	-v $PWD/jellyfin1/cache:/cache \
+	-v /mnt/aliyundrive:/media \
+	-p 8099:8096 \
+	--device=/dev/dri/renderD128 \
+	--device /dev/dri/card0:/dev/dri/card0 \
+	--restart unless-stopped \
+	jellyfin/jellyfin
+
+***************************************************************
+docker run -d --name="gh-proxy-py" \
+	-p 0.0.0.0:80:80 \
+	--restart=always \
+	hunsh/gh-proxy-py:latest
+
+***************************************************************
